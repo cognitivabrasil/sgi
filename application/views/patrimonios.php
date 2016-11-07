92aadb7210162234af814ada57ffd64f604df786
Alan Velasques Santos <alanvelasques@gmail.com>
2016-11-07 14:58:55 -0200

    tabela com linhas em patrimonio

--- application/views/patrimonios.php
+++ application/views/patrimonios.php
@@ -3,19 +3,18 @@
   <p id="titulo_usuario">Patrimônios</p>
   <div id="block_usuario">
     <?php foreach ($data as $row) { ?>
-    <div class="row empreendimento_row">
+    <div class="row patrimonio_row">
       <div class="col-md-4"><?php echo $row->nome; ?></div>
-      <div class="col-md-6"><?php echo $row->responsavel; ?></div>
+      <div class="col-md-4"><?php echo $row->responsavel; ?></div>
       <div class="col-md-2">
-        <div class="visualizar_dados">
+        <span class="sobrescrito_empreendimento"><?php if(isset($row->sala)){ echo $row->sala->data_atribuicao; }else{ echo "Não atribuído"; }?></span>
+      </div>
+      <div class="col-md-2">
           <a href="<?php echo base_url();?>index.php/patrimonios/visualiza/<?php echo $row->id;?>" class="button_action">
-            <button type="button" class="btn btn-default btn-lg">
+            <button type="button" class="btn btn-default btn-xs">
               <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
             </button>
-          </a>
-          <!--<a href="<?php echo base_url();?>index.php/patrimonios/visualiza/<?php echo $row->id;?>">Visualizar dados</a> -->
-        </div>
-        <span class="sobrescrito_empreendimento"><?php if(isset($row->sala)){ echo $row->sala->data_atribuicao; }else{ echo "Não atribuído"; }?></span>
+          </a>        
       </div>
     </div><br>
     <?php  } ?>
