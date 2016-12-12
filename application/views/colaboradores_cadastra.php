6f2de6281b593ae1db0e8e9eb78885b96840f0fe
Alan Velasques Santos <alanvelasques@gmail.com>
2016-12-12 15:16:08 -0200

    Tirado a opção de escolha da empresa que vai ser cadastrado o colaborador e escolhido a empresa automaticamente de acordo com o usuario que esta cadastrando

--- application/views/colaboradores_cadastra.php
+++ application/views/colaboradores_cadastra.php
@@ -4,7 +4,7 @@
     <div id="block_usuario">
       <form method="post" action="<?php echo base_url();?>index.php/colaboradores/insert">
           <input type="text" name="nome" placeholder="Nome">
-          <div style="width:30%; float:right; margin-right:40%;"><input id="habilita_funcao" type="checkbox" name="sem_funcao" style="float:left; width:10px;" value="1" checked><span style="float:left;">Sem função designada</span></div>          
+          <div style="width:30%; float:right; margin-right:40%;"><input id="habilita_funcao" type="checkbox" name="sem_funcao" style="float:left; width:10px;" value="1" checked><span style="float:left;">Sem função designada</span></div>
           <input type="text" name="funcao" placeholder="Função" id="funcao_colaborador">
           <select name="vinculo">
             <option value="0">Vínculo UFRGS</option>
@@ -17,12 +17,13 @@
           <input type="text" name="saida" placeholder="Saída" style="float:left; width:95px;">
           <input type="checkbox" name="em_atividade" style="float:left; width:10px;" value="1"><span style="float:left;">Ainda em atividade</span>
           <input type="checkbox" name="socio" style="float:left; width:10px; clear:both;" value="1"><span style="float:left;">Sócio</span>
-          <select name="empresa" style="clear:both;">
+          <input type="hidden" name="empresa" value="<?php echo $this->session->userdata('id_empreendimento');?>">
+          <!--<select name="empresa" style="clear:both;">
             <option value="0">Empresa</option>
             <?php foreach($data as $row){ ?>
-            <option value="<?php echo $row->id;?>"><?php echo $row->nome_fantasia;?></option>
+            <option value="<?php echo $row->id;?>"><?php echo $row->nome;?></option>
             <?php } ?>
-          </select>
+          </select>--><br><br><br><br>
           <button type="submit" class="btn btn-default btn-lg">
             <span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span> Salvar
           </button>
