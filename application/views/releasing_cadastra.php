06a444497ce3c0f983f7d8ed5448e5cddf6f6279
Alan Velasques Santos <alanvelasques@gmail.com>
2016-12-12 16:11:27 -0200

    removido o dropdown de empresas e setado automaticamente a empresa de acordo com o usuario

--- application/views/releasing_cadastra.php
+++ application/views/releasing_cadastra.php
@@ -4,17 +4,18 @@
     <div id="block_usuario">
       <form method="post" action="<?php echo base_url();?>index.php/releasing/insert" enctype="multipart/form-data">
           <input type="text" name="nome" placeholder="Nome">
-          <select name="empreendimento">
+          <input type="hidden" name="empreendimento" value="<?php echo $this->session->userdata('id_empreendimento');?>">
+          <!--<select name="empreendimento">
             <option value="0">Selecione a empresa</value>
             <option value="1">CEI</value>
             <?php
             foreach ($empresas as $empresa) {
             ?>
-            <option value="<?php echo $empresa->id;?>"><?php echo $empresa->nome_fantasia;?></value>
+            <option value="<?php echo $empresa->id;?>"><?php echo $empresa->nome;?></value>
             <?php
             }
             ?>
-          </select>
+          </select>-->
           <p><span style="float:left; margin-top:7px;">&nbsp;Data: </span><input style="float:left;" type="date" name="data"></p>
           <textarea name="descricao" placeholder="Descrição do evento, prêmio ou acontecimento" style="width:400px; height:100px; clear:both;"></textarea>
           <input type="file" name="imagem">
