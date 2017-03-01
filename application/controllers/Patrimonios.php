38ee13bc932f65d32e2085bc0c273dc9eb5a5c06
Alan Velasques Santos <alanvelasques@gmail.com>
2017-03-01 14:11:48 -0300

    inserção de funções para inserir patrimonio pelo app

--- application/controllers/Patrimonios.php
+++ application/controllers/Patrimonios.php
@@ -295,6 +295,12 @@ class Patrimonios extends CI_Controller {
 
     function insertApp() {
 
+      header('Access-Control-Allow-Origin: *');
+
+      header('Access-Control-Allow-Methods: GET, POST');
+
+      header("Access-Control-Allow-Headers: X-Requested-With");
+
       $this->load->model('patrimonios_model');
 	    $this->patrimonios_model->insert();
 
