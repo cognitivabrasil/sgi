a4d8c15fe2ba537e08447fb53bdcca5a6fa60fa0
Alan Velasques Santos <alanvelasques@gmail.com>
2017-08-16 15:21:10 -0300

    place holder na edicao de patrimonios

--- application/views/patrimonios_edita.php
+++ application/views/patrimonios_edita.php
@@ -5,10 +5,10 @@
       <?php if($this->session->userdata('id_acesso')==1){ ?>
       <form method="post" action="<?php echo base_url();?>index.php/patrimonios/save">
           <input type="hidden" name="id" value="<?php echo $data->id;?>">
-          <input type="text" name="nome" value="<?php echo $data->nome;?>">
-          <input type="text" name="nr" value="<?php echo $data->nrpatrimonio;?>">
-          <input type="text" name="responsavel" value="<?php echo $data->responsavel?>">
-          <input type="text" name="observacoes" value="<?php echo $data->observacoes?>">
+          <input type="text" name="nome" value="<?php echo $data->nome;?>" placeholder="Nome">
+          <input type="text" name="nr" value="<?php echo $data->nrpatrimonio;?>" placeholder="Nr. Patrimônio">
+          <input type="text" name="responsavel" value="<?php echo $data->responsavel?>" placeholder="Responsável na UFRGS">
+          <input type="text" name="observacoes" value="<?php echo $data->observacoes?>" placeholder="Observações">
           <textarea name="descricao" style="width:70%; height:150px;"><?php echo $data->descricao?></textarea>
           <button type="submit" class="btn btn-default btn-lg">
             <span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span> Salvar
